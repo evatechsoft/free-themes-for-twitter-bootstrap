adbf9279700fba77fc771b35623ac8a291ebda25
soundarapandian <soundar.rathinasamy@gmail.com>
2012-12-02 23:44:11 +0530

    Updated Bootbusiness theme.

--- bootbusiness/js/boot-business.js
+++ bootbusiness/js/boot-business.js
@@ -17,7 +17,7 @@ $("document").ready(function() {
 	$(".dropdown-menu li a").mousedown(function() {
 		var dropdown = $(this).parents('.dropdown');
 		var link = dropdown.children(':first-child');
-		link.css('background-color', "#888A85");
+		link.css('background-color', "#2E3436");
 		link.css('color', 'white');
 	});
 	/*End: Prevent the default white background on blur of top navigation */
